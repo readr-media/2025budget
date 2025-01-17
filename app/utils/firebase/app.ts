c4dc630f5b2db1c0d3fd713bcb756ba2675b729f
YuCheTeng <114332515+tyc67@users.noreply.github.com>
2025-01-17 10:10:41 +0800

    initial project

---
+++ app/utils/firebase/app.ts
@@ -0,0 +1,13 @@
+import { initializeApp, getApp } from 'firebase/app'
+import { FIREBASE_CONFIG } from '@/app/constants/config'
+import { getFirestore } from 'firebase/firestore'
+
+export const setup = () => {
+  if (getApp.length) {
+    return getApp()
+  } else {
+    return initializeApp(FIREBASE_CONFIG)
+  }
+}
+const firebaseApp = setup()
+export const db = getFirestore(firebaseApp)

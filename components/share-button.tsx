784ea22fc17c1a6052afe56df18166ba2fd923fa
caesar <caesar.lee@mirrormedia.mg>
2025-01-20 15:42:22 +0800

    fix: share button leak icons when loaded

--- components/share-button.tsx
+++ components/share-button.tsx
@@ -3,7 +3,16 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { ShareButton as ReadrShareButton } from '@readr-media/share-button'
+import { useEffect, useState } from 'react'
 
 export default function ShareButton() {
-  return <ReadrShareButton pathColor="black" direction="vertical" />
+  const [isMounted, setIsMounted] = useState(false)
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+  return (
+    <div className="size-[21px] sm:size-7">
+      {isMounted && <ReadrShareButton pathColor="black" direction="vertical" />}
+    </div>
+  )
 }
